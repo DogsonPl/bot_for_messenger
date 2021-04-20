12d322e6f6643f2a60bdf1f22716dac9e2ba7083
Dogson <dogsonkrul@gmail.com>
2021-04-20 17:19:27 +0200

    fixed disconnecting error in SMPT

--- Bot/sending_emails.py
+++ Bot/sending_emails.py
@@ -14,9 +14,11 @@ async def get_confirmation_code():
 class SmptConnection:
     def __init__(self):
         self.smpt_connection = aiosmtplib.SMTP(hostname=hostname, port=465, use_tls=True)
-        loop.run_until_complete(self.smpt_connection.connect())
-        loop.run_until_complete(self.smpt_connection.ehlo())
-        loop.run_until_complete(self.smpt_connection.login(mail, password))
+
+    async def connect(self):
+        await self.smpt_connection.connect()
+        await self.smpt_connection.ehlo()
+        await self.smpt_connection.login(mail, password)
 
     async def send_mail(self, receiver, code):
         message = MIMEMultipart("alternative")
@@ -31,8 +33,13 @@ Jeśli nie chciałeś połączyć tego maila z botem na Facebooku, zignoruj tego
             return f"✅ Wysłano email z kodem do {receiver}"
         except aiosmtplib.errors.SMTPRecipientsRefused:
             return "🚫 Nie udało się wysłać emaila. Czy na pewno podałeś poprawny email?"
+        except aiosmtplib.errors.SMTPServerDisconnected:
+            await self.connect()
+            await self.smpt_connection.send_message(message)
+            return f"✅ Wysłano email z kodem do {receiver}"
 
 
 loop = asyncio.get_event_loop()
 hostname, mail, password = loop.run_until_complete(parse_config.get_smpt_config())
 smpt_connection = SmptConnection()
+loop.run_until_complete(smpt_connection.connect())
