3d97e94e1a7ad7f0ba3fc2f4fccc556b6923a8b4
Dogson <dogsonkrul@gmail.com>
2021-06-11 19:52:47 +0200

    parse_config.py - changed assertions to ifs

--- Bot/parse_config.py
+++ Bot/parse_config.py
@@ -4,11 +4,10 @@ from configparser import ConfigParser
 async def get_login_data():
     mail = config_parser.get("login", "mail")
     password = config_parser.get("login", "password")
-    try:
-        assert mail != "" and password != ""
-    except AssertionError:
+    if mail == "" or password == "":
         raise Exception("You have to set login data in a config file")
-    return mail, password
+    else:
+        return mail, password
 
 
 async def get_database_config():
@@ -17,22 +16,20 @@ async def get_database_config():
     password = config_parser.get("database", "password")
     database_name = config_parser.get("database", "database_name")
     port = config_parser.get("database", "port")
-    try:
-        assert host != "" and user != "" and database_name != "" and port != ""
-    except AssertionError:
+    if host == "" or user == "" or database_name == "" and port == "":
         raise Exception("You have to set database data in a config file")
-    return host, user, password, database_name, port
+    else:
+        return host, user, password, database_name, port
 
 
 async def get_smpt_config():
     hostname = config_parser.get("smpt", "hostname")
     mail = config_parser.get("smpt", "mail")
     password = config_parser.get("smpt", "password")
-    try:
-        assert hostname != "" and mail != "" and password != ""
-    except AssertionError:
+    if hostname == "" or mail == "" or password == "":
         raise Exception("You have to set SMPT data in a config file")
-    return hostname, mail, password
+    else:
+        return hostname, mail, password
 
 
 config_parser = ConfigParser()
